cde9fcd5215b4ab2ebfbaba1f08f73f688b5fa97
wudyn1 <wudyn1@users.noreply.github.com>
2026-05-15 10:40:31 +0800

    switch to PNG icons (fix PWA install prompt)

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
 var CACHE="rebar-calc-v1";
 self.addEventListener("install",function(e){
-  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(["/rebar-calc/"])}));
+  e.waitUntil(caches.open(CACHE).then(function(c){
+    return c.addAll(["/","/index.html","/icon-192.png","/icon-512.png"]);
+  }));
 });
 self.addEventListener("fetch",function(e){
-  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}));
+  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));
 });
